a1ed8cc2cead0ad79793f56c5fab073bc754e9e8
Clacky <develop@clacky.ai>
2025-01-02 23:13:09 -0500

    Enable comments with sensitive word filtering

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,13 @@
 class Comment < ApplicationRecord
+  # 定义敏感词常量
+  SENSITIVE_WORDS = %w[垃圾 傻逼 操你妈 fuck dick pussy]
+  
   belongs_to :post
 
   validates :name, presence: true
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :content, presence: true, length: { minimum: 4 }
+  validate :check_sensitive_words
 
   def reply_emails
     Comment.where(post_id: self.post_id).collect(&:email).uniq - [ self.email ] - Subscribe.unsubscribe_list - [ ENV['ADMIN_USER'] ]
@@ -18,6 +22,17 @@ class Comment < ApplicationRecord
     if ENV['MAIL_SERVER'].present?
       Rails.logger.info 'comment created, reply worker start'
       NewReplyPostWorker.perform_async(self.id.to_s)
+    end
+  end
+
+  private
+
+  def check_sensitive_words
+    SENSITIVE_WORDS.each do |word|
+      if content&.include?(word)
+        errors.add(:content, :invalid, message: "contains sensitive word")
+        break
+      end
     end
   end
 end
